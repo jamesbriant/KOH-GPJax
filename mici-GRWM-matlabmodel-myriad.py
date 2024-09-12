a9b5b645cfb7839a5fc7d3f6af1e395b7a3d55b0
jamesbriant <james@briant.co.uk>
2024-09-12 16:22:30 +0100

    bug fix

--- mici-GRWM-matlabmodel-myriad.py
+++ mici-GRWM-matlabmodel-myriad.py
@@ -111,6 +111,6 @@ final_states, traces, stats = sampler.sample_chains(
 end_time = process_time()
 elapsed_time = end_time - start_time
 print()
-f = open("elapsed_time_mcmc.txt", "a")
+f = open("elapsed_time_mcmc.txt", "w")
 f.write(f"Elapsed time: {elapsed_time:.3f} seconds.")
 f.close()
