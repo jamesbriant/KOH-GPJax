7a3095434711bc8ae631364519ea5fc1d7a6b622
jamesbriant <james@briant.co.uk>
2024-10-24 17:08:34 +0100

    attempted fix

--- tests/convergence-fixed-N.py
+++ tests/convergence-fixed-N.py
@@ -9,15 +9,42 @@ import numpy as np
 
 import mici
 
+################## Usage ##################
+# python tests/convergence-fixed-N.py n_warm_up_iter n_main_iter hmc_mode [n_steps]
+# e.g.
+# python tests/convergence-fixed-N.py 100 100 dynamic
+# or
+# python tests/convergence-fixed-N.py 100 100 static 3
+
 ################## Parse arguments ##################
-if int(sys.argv[1]) == 0:
-    sampler_name = 'static_3'
-    n_warm_up_iters = [30, 50, 75, 100, 150, 200, 300, 500, 700, 900, 1200]
-elif int(sys.argv[1]) == 1:
+n_warm_up_iter = sys.argv[1]
+n_main_iter = sys.argv[2]
+hmc_mode = sys.argv[3]
+if hmc_mode == 'dynamic':
     sampler_name = 'dynamic'
-    n_warm_up_iters = [25, 30, 45, 60, 80, 100, 125, 150, 200, 270, 350, 500]
+elif hmc_mode == 'static':
+    n_steps = sys.argv[4]
+    sampler_name = f'static-{n_steps}'
 else:
     raise ValueError('Invalid sampler')
+
+################## Parse arguments ##################
+# if int(sys.argv[1]) == 0:
+#     sampler_name = 'dynamic'
+#     # n_warm_up_iters = [30, 50, 75, 100, 150, 200, 300, 500, 700, 900, 1200]
+#     n_warm_up_iters = [50, 75, 100, 150, 200]
+#     # n_main_iter = 400
+#     n_main_iter = 300
+# elif int(sys.argv[1]) == 1:
+#     if len(sys.argv) < 3:
+#         raise ValueError('Please provide number of leapfrog steps')
+#     n_steps = int(sys.argv[2])
+#     sampler_name = f'static-{n_steps}'
+#     # n_warm_up_iters = [30, 50, 75, 100, 150, 200, 300, 500, 700, 900, 1200]
+#     n_warm_up_iters = [50, 75, 100, 150, 200, 300, 500]
+#     n_main_iter = 500
+# else:
+#     raise ValueError('Invalid sampler')
 
 
 ################## Load data ##################
@@ -99,10 +126,10 @@ n_chain = 1
 rng = np.random.default_rng(seed)
 
 ##### Mici sampler and adapters #####
-samplers = {
-    'static_3': mici.samplers.StaticMetropolisHMC(system, integrator, rng, n_step=3),
-    'dynamic': mici.samplers.DynamicMultinomialHMC(system, integrator, rng)
-}
+if hmc_mode == 'dynamic':
+    sampler = mici.samplers.DynamicMultinomialHMC(system, integrator, rng)
+else:
+    sampler = mici.samplers.StaticMetropolisHMC(system, integrator, rng, n_step=n_steps)
 
 adapters = [
     mici.adapters.DualAveragingStepSizeAdapter(0.8),
@@ -123,85 +150,45 @@ def trace_func(state):
     }
 
 
-# n_warm_up_iter = [30, 50, 75, 100, 150, 200, 300, 500, 700, 900, 1200]
-# n_warm_up_iter = [50, 70]
-n_main_iter = 800
-
-################## Iterate over samplers ##################
-# for sampler_name, sampler in samplers.items():
-
-
-# No longer iterating over the samplers
-# Only one sampler is used in this script
-sampler = samplers[sampler_name]
-
 params_transformed_mean = {}
 params_transformed_std = {}
 
-for W in n_warm_up_iters:
-    final_states, traces, stats = sampler.sample_chains(
-        W, 
-        n_main_iter, 
-        init_states, 
-        adapters=adapters, 
-        n_process=n_chain, # only 1 works on MacOS
-        trace_funcs=[trace_func]
-    )
-
-    for var, trace in traces.items():
-        if var == 'hamiltonian':
-            continue
-        var_name = var.split('m_')[1]
-        if var_name == 'theta':
-            params_transformed_mean.setdefault(var_name, []).append(np.mean(mapRto01(trace[0])*(tmax-tmin) + tmin))
-            params_transformed_std.setdefault(var_name, []).append(np.std(mapRto01(trace[0])*(tmax-tmin) + tmin))
-        elif var_name.startswith('ell'):
-            params_transformed_mean.setdefault(var_name, []).append(np.mean(mapRto0inf(trace[0])))
-            params_transformed_std.setdefault(var_name, []).append(np.std(mapRto0inf(trace[0])))
-        elif var_name.startswith('lambda'):
-            params_transformed_mean.setdefault(var_name, []).append(np.mean(mapRto0inf(trace[0])))
-            params_transformed_std.setdefault(var_name, []).append(np.std(mapRto0inf(trace[0])))
-
-    # print(params_transformed_mean)
-
-np.savez(
-    f"convergence/params-transformed-fixed-N-{n_main_iter}-{sampler_name}.npz", 
-    [
-        n_warm_up_iters,
-        params_transformed_mean, 
-        params_transformed_std
-    ]
+final_states, traces, stats = sampler.sample_chains(
+    n_warm_up_iter, 
+    n_main_iter, 
+    init_states, 
+    adapters=adapters, 
+    n_process=n_chain, # only 1 works on MacOS
+    trace_funcs=[trace_func]
 )
 
+for var, trace in traces.items():
+    if var == 'hamiltonian':
+        continue
+    var_name = var.split('m_')[1]
+    # if var_name == 'theta':
+    #     params_transformed_mean.setdefault(var_name, []).append(np.mean(mapRto01(trace[0])*(tmax-tmin) + tmin))
+    #     params_transformed_std.setdefault(var_name, []).append(np.std(mapRto01(trace[0])*(tmax-tmin) + tmin))
+    # elif var_name.startswith('ell'):
+    #     params_transformed_mean.setdefault(var_name, []).append(np.mean(mapRto0inf(trace[0])))
+    #     params_transformed_std.setdefault(var_name, []).append(np.std(mapRto0inf(trace[0])))
+    # elif var_name.startswith('lambda'):
+    #     params_transformed_mean.setdefault(var_name, []).append(np.mean(mapRto0inf(trace[0])))
+    #     params_transformed_std.setdefault(var_name, []).append(np.std(mapRto0inf(trace[0])))
+    if var_name == 'theta':
+        params_transformed_mean[var_name] = np.mean(mapRto01(trace[0])*(tmax-tmin) + tmin)
+        params_transformed_std[var_name] = np.std(mapRto01(trace[0])*(tmax-tmin) + tmin)
+    elif var_name.startswith('ell'):
+        params_transformed_mean[var_name] = np.mean(mapRto0inf(trace[0]))
+        params_transformed_std[var_name] = np.std(mapRto0inf(trace[0]))
+    elif var_name.startswith('lambda'):
+        params_transformed_mean[var_name] = np.mean(mapRto0inf(trace[0]))
+        params_transformed_std[var_name] = np.std(mapRto0inf(trace[0]))
 
-# ################## Save results ##################
-# fig, axes = plt.subplots(4, 2, figsize=(8, 12))
 
-# for i, var in enumerate(params_transformed_mean):
-#     ax = axes.flatten()[i]
-#     ax.set_xscale('log')
-#     ax.plot(n_warm_up_iter, params_transformed_mean[var], 'o-', label='mean')
-#     ax.fill_between(
-#         n_warm_up_iter, 
-#         np.array(params_transformed_mean[var])-np.array(params_transformed_std[var]), 
-#         np.array(params_transformed_mean[var])+np.array(params_transformed_std[var]), 
-#         alpha=0.3
-#     )
-#     ax.set_xlabel('Number of warm-up iterations, W')
-#     ax.set_ylabel('mean')
-
-#     ax2 = ax.twinx()
-#     ax2.plot(n_warm_up_iter, params_transformed_std[var], 'x--', color='tab:orange', label='std')
-#     ax2.set_ylabel('std')
-
-#     ax.legend(loc=2)
-#     ax2.legend(loc=1)
-
-#     ax.set_title(var)
-
-# axes[0,0].axhline(0.4, color='k', linestyle='--')
-# axes[3,0].axhline(400, color='k', linestyle='--')
-# fig.suptitle(f'Convergence of parameters for {sampler_name} sampler\nN={n_warm_up_iter}')
-# plt.tight_layout()
-# plt.savefig(f'convergence/params-convergence-fixed-N-{sampler_name}.png')
-# plt.close()
+np.savez(
+    # f"convergence/{sampler_name}/fixed-N={n_main_iter}/W={n_warm_up_iter}.npz", 
+    f"convergence/W={n_warm_up_iter}-N={n_main_iter}.npz",
+    params_transformed_mean=params_transformed_mean, 
+    params_transformed_std=params_transformed_std
+)
