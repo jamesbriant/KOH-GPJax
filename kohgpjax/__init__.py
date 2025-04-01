89232701293333b4bcd5da02303fbb39c5cbbe41
jamesbriant <james@briant.co.uk>
2025-04-01 17:47:33 +0100

    Bump version to 0.2.0 and update GPJax dependency to be less than 0.9 for backward compatibility

--- kohgpjax/__init__.py
+++ kohgpjax/__init__.py
@@ -8,7 +8,7 @@ from kohgpjax.dataset import KOHDataset
 __description__ = "Bayesian calibration from Kennedy & O'Hagan (2001) implementation in GPJax"
 __url__ = "https://github.com/jamesbriant/KOH-GPJax"
 __contributors__ = "James Briant - https://james.briant.co.uk"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "base",
