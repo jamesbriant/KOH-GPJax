8a7c704be1b49f0f3abbe1ea75a7178833bdd6ab
jamesbriant <james@briant.co.uk>
2025-04-14 15:52:34 +0100

    clean up imports

--- kohgpjax/__init__.py
+++ kohgpjax/__init__.py
@@ -1,10 +1,10 @@
 from kohgpjax import (
-    kohmodel,
     gps,
     kernels,
 )
 from kohgpjax.dataset import KOHDataset
 from kohgpjax.parameters import ModelParameters, ParameterPrior
+from kohgpjax.kohmodel import KOHModel
 
 __description__ = "Bayesian calibration from Kennedy & O'Hagan (2001) implementation in GPJax"
 __url__ = "https://github.com/jamesbriant/KOH-GPJax"
