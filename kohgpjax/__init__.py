77ed7f38f981fd79a7f3ff316015c0a1d4dd6a01
jamesbriant <james@briant.co.uk>
2025-04-25 15:59:12 +0100

    Update version

--- kohgpjax/__init__.py
+++ kohgpjax/__init__.py
@@ -9,7 +9,7 @@ from kohgpjax.kohmodel import KOHModel
 __description__ = "Bayesian calibration from Kennedy & O'Hagan (2001) implementation in GPJax"
 __url__ = "https://github.com/jamesbriant/KOH-GPJax"
 __contributors__ = "James Briant - https://james.briant.co.uk"
-__version__ = "0.3.1"
+__version__ = "0.3.0"
 
 __all__ = [
     "base",
